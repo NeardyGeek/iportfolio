a6a5535e62bf8e97b45b9b28bc3250b38fd33f0e
gen li <38142924+NeardyGeek@users.noreply.github.com>
2020-01-22 18:24:38 -0500

    Updates

---
+++ precache-manifest.6347d5ee94e64ac19e8bf01dff3c31b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b26c19481d24eced121e1917b878f08",
+    "url": "/iportfolio/index.html"
+  },
+  {
+    "revision": "10b5e3b697fc0f9ce7f2",
+    "url": "/iportfolio/static/css/main.4ac79d1e.chunk.css"
+  },
+  {
+    "revision": "afb020ebe50495a8881e",
+    "url": "/iportfolio/static/js/2.6759e69d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iportfolio/static/js/2.6759e69d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10b5e3b697fc0f9ce7f2",
+    "url": "/iportfolio/static/js/main.456df2d2.chunk.js"
+  },
+  {
+    "revision": "f8fd37535618e13ba951",
+    "url": "/iportfolio/static/js/runtime-main.28dec42b.js"
+  },
+  {
+    "revision": "4ab98db0e04a32c12fc41498f68ad1e4",
+    "url": "/iportfolio/static/media/404.4ab98db0.png"
+  },
+  {
+    "revision": "6a25b013d8c19ddd002f898928052314",
+    "url": "/iportfolio/static/media/android.6a25b013.jpg"
+  },
+  {
+    "revision": "b571170548d649a52121dea33c729a03",
+    "url": "/iportfolio/static/media/image.b5711705.png"
+  },
+  {
+    "revision": "bf07882e6593a4b128aea228bca910f4",
+    "url": "/iportfolio/static/media/profile.bf07882e.jpg"
+  }
+]);
